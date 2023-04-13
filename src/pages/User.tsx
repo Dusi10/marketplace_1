2a275c839f49c3a400cb0a3a7c0f27ca4361294e
Végh Ádám <97345798+Dusi10@users.noreply.github.com>
2023-04-13 14:26:33 +0200

    Likes/Dislikes component (#1)

--- src/pages/User.tsx
+++ src/pages/User.tsx
@@ -1,8 +1,10 @@
+import { Likes } from "../components/Likes/Dislikes";
 import { Rating } from "../components/rating";
 
 export const User = () => {
   return <>
   User Page
   <Rating/>
+  <Likes/>
   </>;
 };
